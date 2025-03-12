7bd25c1117ee0c8b52110d34ca313df45b0e632d
Aokifree20 <147191684+Aokifree20@users.noreply.github.com>
2025-03-11 22:25:18 -0500

    Actualizacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { CharacterModule } from './character/character.module';
 
-import { AuthModule } from './auth/auth.module';
+
 dotenv.config();
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL),
     CharacterModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
